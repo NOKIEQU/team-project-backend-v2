06d6b686b88e3576ddcf76810d730b58007e8bef
NOKIEQU <wiktorp92@gmail.com>
2024-12-06 14:21:06 +0000

    login system done

--- src/server.js
+++ src/server.js
@@ -5,9 +5,6 @@ const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const reviewRoutes = require('./routes/reviewRoutes');
-const cartRoutes = require('./routes/cartRoutes');
 const genreRoutes = require('./routes/genreRoutes');
 
 const errorHandler = require('./middleware/errorHandler');
@@ -19,14 +16,10 @@ const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, '../public/images')));
 
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/reviews', reviewRoutes);
-app.use('/api/cart', cartRoutes);
 app.use('/api/genres', genreRoutes);
 
 // Error handling middleware
