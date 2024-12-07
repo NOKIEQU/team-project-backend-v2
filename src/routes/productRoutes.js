a17277d297e2dda2b5c8d567ecad0cd4adc880ac
NOKIEQU <wiktorp92@gmail.com>
2024-12-07 02:52:51 +0000

    deleted upload picture function, for now

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { body } = require('express-validator');
 const productController = require('../controllers/productController');
 const { auth, adminAuth } = require('../middleware/auth');
-const upload = require('../middleware/upload');
 
 const router = express.Router();
 
@@ -16,7 +15,7 @@ router.post('/', adminAuth, [
   body('rating').isFloat({ min: 0, max: 5 }),
   body('releaseYear').isInt({ min: 1800, max: new Date().getFullYear() }),
   body('genreId').notEmpty(),
-], upload.array('product', 5), productController.createProduct);
+]);
 
 router.patch('/:id', adminAuth, [
   body('title').optional().notEmpty(),
@@ -25,7 +24,7 @@ router.patch('/:id', adminAuth, [
   body('rating').optional().isFloat({ min: 0, max: 5 }),
   body('releaseYear').optional().isInt({ min: 1800, max: new Date().getFullYear() }),
   body('genreId').optional().notEmpty(),
-], upload.array('product', 5), productController.updateProduct);
+]);
 
 router.delete('/:id', adminAuth, productController.deleteProduct);
 
