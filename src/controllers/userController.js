71687afc999fe30787d265c3981cf71424803fd8
NOKIEQU <wiktorp92@gmail.com>
2025-03-24 03:52:17 +0000

    edit update user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -126,12 +126,35 @@ exports.getAllUsers = async (req, res) => {
 };
 
 exports.updateUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['firstName', "lastName", 'email', 'password', 'role']; 
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).json({ error: 'Invalid updates!' });
+  }
+
   try {
-    const { name, email, role } = req.body;
-    const user = await prisma.user.update({
-      where: { id: req.params.id },
-      data: { name, email, role },
+    const user = req.user;
+
+    updates.forEach((update) => {
+      if (update === 'password') {
+        user[update] = bcrypt.hashSync(req.body[update], 8);
+      } else {
+        user[update] = req.body[update];
+      }
     });
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: user,
+    });
+
     res.json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
